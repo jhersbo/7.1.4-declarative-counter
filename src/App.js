753208c185f6a79a2c36df6d5f75c2d385adc6d5
jhersbo <jhersbo@gmail.com>
2022-01-06 11:07:15 -0600

    assignment upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
+  )
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React!
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
+
+export default App;
